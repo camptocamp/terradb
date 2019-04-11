47ec9db89518a11cdbfaa7c273202b0f9fd9f8bf
Léo Depriester <leo.depriester@camptocamp.com>
2019-04-11 08:11:04 +0200

    add basic cli

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gorilla/mux v1.7.1
 	github.com/hashicorp/terraform v0.11.13
+	github.com/jessevdk/go-flags v1.4.0
 	github.com/sirupsen/logrus v1.4.1
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
@@ -15,7 +16,7 @@ require (
 	golang.org/x/lint v0.0.0-20190409202823-959b441ac422 // indirect
 	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
 	golang.org/x/sync v0.0.0-20190227155943-e225da77a7e6 // indirect
-	golang.org/x/sys v0.0.0-20190405154228-4b34438f7a67 // indirect
-	golang.org/x/tools v0.0.0-20190409223705-96f2e7ef861b // indirect
+	golang.org/x/sys v0.0.0-20190410170021-cc4d4f50624c // indirect
+	golang.org/x/tools v0.0.0-20190410211219-2538eef75904 // indirect
 	labix.org/v2/mgo v0.0.0-20140701140051-000000000287
 )
--- main.go
+++ main.go
@@ -1,40 +1,59 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/jessevdk/go-flags"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/camptocamp/terradb/internal/api"
 	"github.com/camptocamp/terradb/internal/storage"
 )
 
+var opts struct {
+	Version bool `short:"v" long:"version" description:"Display version."`
+	MongoDB struct {
+		URL      string `long:"mongodb-url" description:"MongoDB URL" env:"MONGODB_URL"`
+		Username string `long:"mongodb-username" description:"MongoDB Username" env:"MONGODB_USERNAME"`
+		Password string `long:"mongodb-password" description:"MongoDB Password" env:"MONGODB_PASSWORD"`
+	} `group:"MongoDB options"`
+	API struct {
+		Address string `long:"api-address" description:"Address on to bind the API server" env:"API_ADDRESS" default:"127.0.0.1"`
+		Port    string `long:"api-port" description:"Port on to listen" env:"API_PORT" default:"8080"`
+	} `group:"API server options"`
+}
+
+var VERSION = "undefined"
+
 func main() {
 	var st storage.Storage
 
-	st, err := storage.NewMongoDB(&storage.MongoDBConfig{
-		URL:      "mongodb://localhost:27017",
-		Username: "root",
-		Password: "root",
+	parser := flags.NewParser(&opts, flags.Default)
+	_, err := parser.Parse()
+	if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+		os.Exit(0)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if opts.Version {
+		fmt.Printf("TerraDB v%v\n", VERSION)
+		os.Exit(0)
+	}
+
+	st, err = storage.NewMongoDB(&storage.MongoDBConfig{
+		URL:      opts.MongoDB.URL,
+		Username: opts.MongoDB.Username,
+		Password: opts.MongoDB.Password,
 	})
 	if err != nil {
 		log.Fatalf("failed to setup storage: %s", err)
 	}
 
 	api.StartServer(&api.API{
-		Address: "0.0.0.0",
-		Port:    "8080",
+		Address: opts.API.Address,
+		Port:    opts.API.Port,
 	}, st)
-
-	/*
-		log.Info(st.GetName())
-
-		rawDocument := `{"id": "1111", "foo": "bar"}`
-
-		var structuredDocument interface{}
-		json.Unmarshal([]byte(rawDocument), &structuredDocument)
-
-		err = st.InsertTFState(structuredDocument, time.Now(), "direct")
-		if err != nil {
-			log.Fatalf("failed to insert document: %s", err)
-		}
-	*/
 }
