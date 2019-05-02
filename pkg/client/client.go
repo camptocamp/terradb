c0aa59cf61732ac4ab0bc44717a4db2d7d479beb
Raphaël Pinson <raphael.pinson@camptocamp.com>
2019-05-02 11:32:09 +0200

    Refactor/lint client lib

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -8,18 +8,18 @@ import (
 	"github.com/camptocamp/terradb/internal/storage"
 )
 
-// Client is a TerraDB client
+// Client is a TerraDB client.
 type Client struct {
 	httpClient *http.Client
 	URL        string
 }
 
-// NewClient returns a new TerraDB client from its URL
+// NewClient returns a new TerraDB client from its URL.
 func NewClient(url string) *Client {
 	return &Client{&http.Client{}, url}
 }
 
-// ListStates lists all state names in TerraDB
+// ListStates lists all state names in TerraDB.
 func (c *Client) ListStates() (states storage.StateCollection, err error) {
 	err = c.get(&states, "states", nil)
 	if err != nil {
@@ -29,8 +29,8 @@ func (c *Client) ListStates() (states storage.StateCollection, err error) {
 	return
 }
 
-// GetState returns a TerraDB state from its name and serial
-// Use 0 as serial to return the latest version of the state
+// GetState returns a TerraDB state from its name and serial.
+// Use 0 as serial to return the latest version of the state.
 func (c *Client) GetState(name string, serial int) (st storage.State, err error) {
 	params := map[string]string{
 		"serial": fmt.Sprintf("%v", serial),
@@ -44,11 +44,21 @@ func (c *Client) GetState(name string, serial int) (st storage.State, err error)
 	return
 }
 
-// ListStateSerials lists all state serials and last_modified times for a given name
+// ListStateSerials lists all state serials and last_modified times for a given name.
 func (c *Client) ListStateSerials(name string) (coll storage.StateCollection, err error) {
 	err = c.get(&coll, "states/"+name+"/serials", nil)
 	if err != nil {
 		return coll, fmt.Errorf("failed to retrieve state serials: %v", err)
+	}
+
+	return
+}
+
+// GetResource returns a TerraDB resource from its state, module and name.
+func (c *Client) GetResource(state, module, name string) (st storage.Resource, err error) {
+	err = c.get(&st, "resources/"+state+"/"+module+"/"+name, nil)
+	if err != nil {
+		return st, fmt.Errorf("failed to retrieve resource: %v", err)
 	}
 
 	return
@@ -79,14 +89,4 @@ func (c *Client) get(v interface{}, path string, params map[string]string) error
 	json.NewDecoder(resp.Body).Decode(&v)
 
 	return err
-}
-
-// GetResource returns a TerraDB resource from its state, module and name
-func (c *Client) GetResource(state, module, name string) (st storage.Resource, err error) {
-	err = c.get(&st, "resources/"+state+"/"+module+"/"+name, nil)
-	if err != nil {
-		return st, fmt.Errorf("failed to retrieve resource: %v", err)
-	}
-
-	return
 }
